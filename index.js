c60d7c2162e333b559f5e9658a8b1acc42c247ee
ysugimoto <sugimoto@wnotes.net>
2018-05-18 17:47:08 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const _buildJS = require('./lib/build-js.js');
+const _buildCSS = require('./lib/build-css.js');
+
+const _mapper = files => {
+  return bundles => {
+    return bundles.reduce((prev, next, index) => {
+      prev[files[index]] = next;
+      return prev;
+    }, {});
+  };
+};
+
+module.exports = {
+  buildJS: (...files) => _buildJS(...files).then(_mapper(files)),
+  bundleCSS: (...files) => _buildCSS(...files).then(_mapper(files))
+};
